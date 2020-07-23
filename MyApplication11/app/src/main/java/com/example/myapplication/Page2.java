defe194234a37fe2b14202c193a5f2992399e60a
frankenstein256 <nergin.turgut@hotmail.com>
2020-07-23 12:07:43 +0300

    First commit

---
+++ MyApplication11/app/src/main/java/com/example/myapplication/Page2.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+        Intent path = getIntent();
+
+        String message = path.getStringExtra("path");
+        Toast.makeText(this, message.substring(2), Toast.LENGTH_SHORT).show();
+
+    }
+
+}
